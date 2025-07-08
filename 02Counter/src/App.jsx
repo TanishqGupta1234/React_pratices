226e5c41cdbbbd711740539818ba891a0d1084b2
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-07-09 00:06:24 +0530

    my wokrs

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -1,38 +1,33 @@
-
-import './App.css'
+import './App.css';
 import { useState } from 'react';
+
 function App() {
+  const [counter, setCounter] = useState(15); // use camelCase
 
-  const [Counter, setCounter] = useState(15)
+  const addValues = () => {
+    setCounter(counter + 1); // Increments state
+  };
 
-  let counter = 5;
-
-  const addValues = () =>{
-    // counter = counter + 1
-    // console.log(counter);
-    setCounter(counter + 1)
-  }
-  const reomveValues = () =>{
-    // counter = counter + 1
-    // console.log(counter);
-    setCounter(counter - 1)
-  }
-
-
+  const removeValues = () => {
+    setCounter(counter - 1); // Decrements state
+  };
 
   return (
     <>
-    <h1>tanishq</h1>
-    <h1>Counter : {counter}</h1>
+      <h1>Tanishq</h1>
+      <h2>Counter: {counter}</h2>
 
-    <button onClick={addValues}
-    >Add Values</button>
-    <br />
-   <button onClick={reomveValues}
-   >Remove values</button>
+      <button onClick={addValues}>
+        Add Value
+      </button>
 
+      <br /><br />
+
+      <button onClick={removeValues}>
+        Remove Value
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
